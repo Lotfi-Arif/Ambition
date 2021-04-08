72399e17f2d650cce639dc96dd1aeee0191eba92
Lotfi Arif <luffyarif58@gmail.com>
2021-04-08 22:33:52 +0800

    Added a working layouts folder

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ const users = []
 
 app.set('view engine', 'ejs')
 app.set('views', __dirname + '/views')
-    // app.set('layout', 'layouts/layout')
+app.set('layout', 'layouts/layout')
 app.use(expressLayouts)
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: false }))
