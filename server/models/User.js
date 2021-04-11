0fab552ceef85d04a3617304d99fe71743077884
Lotfi Arif <anwar.l@graduate.utm.my>
2021-04-11 22:28:12 +0800

    Added validation for data in user model

--- server/models/User.js
+++ server/models/User.js
@@ -1,10 +1,28 @@
 const mongoose = require('mongoose')
+const uniqueValidator = require('mongoose-unique-validator');
+const crypto = require('crypto');
 
-const userSchema = new mongoose.Schema({
-    username: String,
-    email: String,
-    bio: String,
-    image: String,
+
+const UserSchema = new mongoose.Schema({
+    username: { type: String, lowercase: true, unique: true, required: [true, "can't be blank"], match: [/^[a-zA-Z0-9]+$/, 'is invalid'], index: true },
+    fullname: String,
+    email: { type: String, lowercase: true, unique: true, required: [true, "can't be blank"], match: [/\S+@\S+\.\S+/, 'is invalid'], index: true },
     hash: String,
+    address: String,
+    phoneNo: String,
     salt: String
-}, {timestamps: true})
+}, { timestamps: true })
+
+UserSchema.plugin(uniqueValidator, { message: 'is already taken.' });
+
+UserSchema.methods.validPassword = function(password) {
+    const hash = crypto.pbkdf2Sync(password, this.salt, 10000, 512, 'sha512').toString('hex');
+    return this.hash === hash;
+};
+
+UserSchema.methods.setPassword = function(password) {
+    this.salt = crypto.randomBytes(16).toString('hex');
+    this.hash = crypto.pbkdf2Sync(password, this.salt, 10000, 512, 'sha512').toString('hex');
+};
+
+module.exports = mongoose.model('User', UserSchema);
